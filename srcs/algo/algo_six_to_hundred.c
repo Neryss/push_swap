e5eec8cdea6d9912980b0710d9df2d2c98ca00ce
ckurt <ckurt@student.42lyon.fr>
2021-03-26 14:03:37 +0100

    marche pas

--- srcs/algo/algo_six_to_hundred.c
+++ srcs/algo/algo_six_to_hundred.c
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 11:51:59 by ckurt             #+#    #+#             */
-/*   Updated: 2021/03/26 13:44:03 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2021/03/26 14:03:16 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,51 +54,16 @@ void	sort_more_median(t_swapper *swapper)
 {
 	t_smallest	s_b;
 	t_biggest	b_b;
+	int			rotate;
 
 	push_more_median(swapper);
+	rotate = 0;
 	while (swapper->stack_b.size)
 	{
 		s_b = find_smallest_b(swapper);
 		b_b = find_biggest(swapper->stack_b);
-		if (s_b.index < b_b.index)
-		{
-			while (s_b.index > 0)
-			{
-				do_move(swapper, "rb");
-				s_b.index--;
-			}
-			do_move(swapper, "pa");
-			do_move(swapper, "ra");
-		}
-		else
-		{
-			while (b_b.index < swapper->stack_b.size + 1)
-			{
-				do_move(swapper, "rrb");
-				b_b.index++;
-			}
-			do_move(swapper, "pa");
-			do_move(swapper, "ra");
-		}
-	}
-}
-
-void	sort_less_median(t_swapper *swapper)
-{
-	t_smallest	s_b;
-	t_biggest	b_b;
-
-	push_less_median(swapper);
-	while (swapper->stack_b.size)
-	{
-		s_b = find_smallest_b(swapper);
-		b_b = find_biggest(swapper->stack_b);
-		printf("smallest | %d | id : %d |\n", s_b.nb, s_b.index);
-		printf("biggest  | %d | id : %d |\n", b_b.nb, b_b.index);
-		display_stacks(swapper);
 		if (s_b.index <= b_b.index)
 		{
-			printf("yo\n");
 			while (s_b.index > 0)
 			{
 				do_move(swapper, "rb");
@@ -125,12 +90,71 @@ void	sort_less_median(t_swapper *swapper)
 					b_b.index++;
 				}
 			}
+			rotate++;
 			do_move(swapper, "pa");
 		}
 	}
-			display_stacks(swapper);
-			while (1)
-				;
+	while (rotate)
+	{
+		do_move(swapper, "ra");
+		rotate--;
+	}
+	// display_stacks(swapper);
+	// 		while (1)
+	// 			;
+}
+
+void	sort_less_median(t_swapper *swapper)
+{
+	t_smallest	s_b;
+	t_biggest	b_b;
+	int			rotate;
+
+	push_less_median(swapper);
+	rotate = 0;
+	while (swapper->stack_b.size)
+	{
+		s_b = find_smallest_b(swapper);
+		b_b = find_biggest(swapper->stack_b);
+		display_stacks(swapper);
+		if (s_b.index <= b_b.index)
+		{
+			printf("yo\n");
+			while (s_b.index > 0)
+			{
+				do_move(swapper, "rb");
+				s_b.index--;
+			}
+			do_move(swapper, "pa");
+			do_move(swapper, "ra");
+			rotate++;
+		}
+		else
+		{
+			if (b_b.index < swapper->stack_b.size / 2)
+			{
+				while (b_b.index > 0)
+				{
+					do_move(swapper, "ra");
+					b_b.index--;
+				}
+			}
+			else
+			{
+				while (b_b.index < swapper->stack_b.size + 1)
+				{
+					do_move(swapper, "rra");
+					b_b.index++;
+				}
+			}
+			do_move(swapper, "pa");
+		}
+	}
+	while (rotate > 0)
+	{
+		do_move(swapper, "rra");
+		rotate--;
+	}
 }
 
 
