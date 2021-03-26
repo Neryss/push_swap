0a80cb7b518c0dadba478f6c3a1d0ee759649f64
ckurt <ckurt@student.42lyon.fr>
2021-03-26 13:44:49 +0100

    patched first part

--- srcs/algo/algo_six_to_hundred.c
+++ srcs/algo/algo_six_to_hundred.c
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 11:51:59 by ckurt             #+#    #+#             */
-/*   Updated: 2021/03/26 13:16:22 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2021/03/26 13:44:03 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,27 +93,44 @@ void	sort_less_median(t_swapper *swapper)
 	{
 		s_b = find_smallest_b(swapper);
 		b_b = find_biggest(swapper->stack_b);
-		if (s_b.index < b_b.index)
+		printf("smallest | %d | id : %d |\n", s_b.nb, s_b.index);
+		printf("biggest  | %d | id : %d |\n", b_b.nb, b_b.index);
+		display_stacks(swapper);
+		if (s_b.index <= b_b.index)
 		{
+			printf("yo\n");
 			while (s_b.index > 0)
 			{
 				do_move(swapper, "rb");
 				s_b.index--;
 			}
 			do_move(swapper, "pa");
+			do_move(swapper, "ra");
 		}
 		else
 		{
-			while (b_b.index < swapper->stack_b.size + 1)
+			if (b_b.index < swapper->stack_b.size / 2)
 			{
-				do_move(swapper, "rrb");
-				b_b.index++;
+				while (b_b.index > 0)
+				{
+					do_move(swapper, "ra");
+					b_b.index--;
+				}
+			}
+			else
+			{
+				while (b_b.index < swapper->stack_b.size + 1)
+				{
+					do_move(swapper, "rra");
+					b_b.index++;
+				}
 			}
 			do_move(swapper, "pa");
-			do_move(swapper, "ra");
 		}
 	}
-	// display_stacks(swapper);
+			display_stacks(swapper);
+			while (1)
+				;
 }
 
 
@@ -121,6 +138,5 @@ void	six_to_hundreds(t_swapper *swapper)
 {
 	sort_less_median(swapper);
 	sort_more_median(swapper);
-	// do_move(swapper, "ra");
-	// display_stacks(swapper);
+	// do_move(swapper, "sa");
 }
